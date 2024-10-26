67a53ba0216e0bc3222289f97b25e12f14fee919
Filip Juza <filip.juza.2000@gmail.com>
2024-10-26 16:32:40 +0200

    add search by full name for athletes

--- queries/athlete.go
+++ queries/athlete.go
@@ -2,10 +2,26 @@ package queries
 
 import (
 	"net/http"
+	"strings"
 
 	"gorm.io/gorm"
 )
 
 func GetAthletesQuery(db *gorm.DB, r *http.Request) *gorm.DB {
-	return db.Preload("Disciplines")
+	db = db.Preload("Disciplines")
+
+	queryFunctions := []func(db *gorm.DB) *gorm.DB{}
+	queryParams := r.URL.Query()
+
+	if queryParams.Has("search") {
+		searchTerm := "%" + strings.ToLower(queryParams.Get("search")) + "%"
+		queryFunctions = append(queryFunctions, func(db *gorm.DB) *gorm.DB {
+			return db.Where("first_name || ' ' || last_name LIKE ? OR last_name || ' ' || first_name LIKE ?", searchTerm, searchTerm)
+		})
+	}
+
+	// below is the way to fetch the user from the context
+	// user := r.Context().Value(utils.UserContextKey).(models.User)
+
+	return db.Scopes(queryFunctions...)
 }
