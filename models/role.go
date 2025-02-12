40cb1b0c1e6ebfa7d1a3adfe6c225cd812c014b4
Filip Juza <filip.juza.2000@gmail.com>
2025-02-12 22:42:42 +0100

    fix tests

--- models/role.go
+++ models/role.go
@@ -2,6 +2,6 @@ package models
 
 type Role struct {
 	AppModel
-	Name  string `json:"name" validate:"required,one_of=admin user organizer" gorm:"not null;unique"`
+	Name  string `json:"name" validate:"required,oneof=admin user organizer" gorm:"not null;unique"`
 	Users []User `json:"users" gorm:"many2many:user_roles;constraint:OnDelete:CASCADE"`
 }
