bab9e73efe940c4a969ae2db337d805b965ca4d7
Filip Juza <filip.juza.2000@gmail.com>
2025-04-09 11:14:47 +0200

    fix typo in username jwt

--- controllers/auth.go
+++ controllers/auth.go
@@ -98,10 +98,10 @@ func Login() utils.HandlerWithError {
 			}
 
 			token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-				"sub":       user.ID,
-				"exp":       time.Now().Add(jwtTokenExpiration).Unix(),
-				"roles":     roleNames,
-				"usernamae": user.Username,
+				"sub":      user.ID,
+				"exp":      time.Now().Add(jwtTokenExpiration).Unix(),
+				"roles":    roleNames,
+				"username": user.Username,
 			})
 
 			tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SIGNING_SECRET")))
